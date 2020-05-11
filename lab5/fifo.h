90bf7f39e567851d799875b125d5a0f4fb612ad5
BuildTools <thetensymbols@gmail.com>
2020-05-12 00:34:02 +0300

    5

--- lab5/fifo.h
+++ lab5/fifo.h
@@ -8,16 +8,16 @@ template <class T>
 class FIFO {
 private:
 
-	std::vector<T> queue;
+	std::vector<T> data;
 public:
-	void push(const T&) {
-
+	void push(const T& element) {
+		data.push_back(element);
 	}
 	void pop() {
-
+		data.erase(data.begin());
 	}
 	const T& top() {
-
+		return data[data.size() - 1];
 	}
 };
 
