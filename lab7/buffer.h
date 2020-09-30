c2396767c6b7dafe0fee32b457189b2f084fd7dd
BuildTools <thetensymbols@gmail.com>
2020-09-30 23:32:21 +0300

    buff

--- lab7/buffer.h
+++ lab7/buffer.h
@@ -1,1 +1,12 @@
 #pragma once
+
+template<class T> 
+class cBuffer {
+public:
+	explicit cBuffer(size_t capacity);
+
+private:
+	T* ptr_;
+	size_t capacity;
+	size_t currSize;
+};
--- lab7/main.cpp
+++ lab7/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
+
 int main() {
 
+	
 
 }
